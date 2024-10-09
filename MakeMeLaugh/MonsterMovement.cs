227aadd48ebcb20dd86e7df136869bd44424d90d
Luc-van-Kessel <113196311+Luc-van-Kessel@users.noreply.github.com>
2024-10-09 10:20:13 +0200

    Update MonsterMovement.cs

--- MakeMeLaugh/MonsterMovement.cs
+++ MakeMeLaugh/MonsterMovement.cs
@@ -63,8 +63,6 @@ public class MonsterMovement : MonoBehaviour
                     hasDestroyedTimelineObject = true;
                 }
             }
-            
-           
         }
     }
 }
