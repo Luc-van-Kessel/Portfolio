8ec50a4216f8d291eee88910cda8138bdf225d35
Luc-van-Kessel <113196311+Luc-van-Kessel@users.noreply.github.com>
2024-11-05 15:44:09 +0100

    Update PickUpUI.cs

--- MarbleGame/PickUpUI.cs
+++ MarbleGame/PickUpUI.cs
@@ -3,7 +3,7 @@ using TMPro;
 public class PickUpUI : MonoBehaviour
 {
     private TextMeshProUGUI text;
-    // Start is called before the first frame update
+
     void Start()
     {
         text = GetComponent<TextMeshProUGUI>();
