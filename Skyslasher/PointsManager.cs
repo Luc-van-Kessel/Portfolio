7f57ead7e168390e406fdbc4fbf89369c8fba35b
Luc-van-Kessel <113196311+Luc-van-Kessel@users.noreply.github.com>
2024-11-05 16:58:30 +0100

    Update PointsManager.cs

--- Skyslasher/PointsManager.cs
+++ Skyslasher/PointsManager.cs
@@ -1,11 +1,9 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+
 public class PointsManager : MonoBehaviour
 {
     public List<EnemyStats> enemyStatsList;
-
     private float totalPoints;
 
     public delegate void PointsChanged(float newPoints);
